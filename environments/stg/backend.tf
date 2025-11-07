47ab55d71535c880f253e669b47c8e8e69a9bc9c
Mohammad Irfan <irfanmd40351@gmail.com>
2025-11-07 15:06:00 +0530

    Update Azure backend configuration for staging

--- environments/stg/backend.tf
+++ environments/stg/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "STG-TF-RG"
-    storage_account_name = "rapidztfstg"
+    resource_group_name  = "Voltica-stg-tf-rg"
+    storage_account_name = "volticatfstg771"
     container_name       = "tfstatestg"
-    key                  = "stg.terraform.tfstate" 
+    key                  = "stg.terraform.tfstate"  
   }
 }
