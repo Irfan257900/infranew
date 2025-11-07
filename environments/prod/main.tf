46cbd50f1bbc9efc52d0d01628e6b8f3e0c3ff53
Mohammad Irfan <irfanmd40351@gmail.com>
2025-11-07 14:57:18 +0530

    Rename project from Paybase to Voltica
    Updated project name from 'Paybase' to 'Voltica' and modified associated resources accordingly.

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,32 +1,28 @@
-
 locals {
-  env         = "prod"
-  project     = "Paybase"
+  env       = "prod"
+  project   = "Voltica" # Project is now Voltica
   common_tags = { "Business-owners" = "Project Manager", "Core-function" = "application", "Criticality" = "1", "Environment" = local.env, "Primary-application" = local.project, "Technical-owner" = "DevOps Team" }
 
-  # This block defines the names for container-based App Services
+  # All app service names and docker paths are now based on "Voltica"
   app_service_docker_images = {
-    "Paybaseprd-admin"       = "Paybase/admin-app"
-    "Paybasesprd-signalR" = "Paybase/signal-app"
-    "Paybaseprd-coreapi" = "Paybase/coreapi-app"
-    "Paybaseprd-cardsapi" = "Paybase/cardsapi-app"
-    "Paybaseprd-banksapi" = "Paybase/banksapi-app"
-    "Paybaseprd-paymentsapi" = "Paybase/paymentsapi-app"
-    "Paybaseprd-paylinks" = "Paybase/paylinks-app"
-    "Paybaseprd" = "Paybase/user-app"
+    "volticaprd-admin"       = "voltica/admin-app"
+    "volticaprd-signalR"     = "voltica/signal-app"
+    "volticaprd-coreapi"     = "voltica/coreapi-app"
+    "volticaprd-cardsapi"    = "voltica/cardsapi-app"
+    "volticaprd-banksapi"    = "voltica/banksapi-app"
+    "volticaprd-paymentsapi" = "voltica/paymentsapi-app"
+    "volticaprd-paylinks"    = "voltica/paylinks-app"
+    "volticaprd-user"        = "voltica/user-app"
   }
   
-
+  # All function names are now based on "Voltica"
   function_app_names = [
-    "PaybaseMarketdataprd",
-    "Paybasesubscriberprd",
-    "PaybaseSweepfunctionprd"
+    "VolticaMarketdataprd",
+    "Volticasubscriberprd",
+    "VolticaSweepfunctionprd"
   ]
 }
 
-# ... (the rest of your main.tf file)
-
-# Data source to get the current identity running Terraform
 data "azurerm_client_config" "current" {}
 
 # --- RESOURCE GROUPS ---
@@ -52,7 +48,6 @@ resource "azurerm_resource_group" "security_rg" {
 }
 
 # --- MODULES ---
-
 module "networking" {
   source = "../../modules/networking"
   resource_group_name = azurerm_resource_group.network_rg.name
@@ -61,92 +56,98 @@ module "networking" {
   vnet_name           = "${local.project}-${local.env}-vnet"
   vnet_address_space  = ["10.10.0.0/16"]
   subnets = {
-    IntegrationvmSubnet       = { address_prefixes = ["10.10.10.0/24"] }
+    IntegrationvmSubnet    = { address_prefixes = ["10.10.10.0/24"] }
     PrivateEndpointsSubnet = { address_prefixes = ["10.10.11.0/24"] }
     sqlVmSubnet            = { address_prefixes = ["10.10.12.0/24"] }
   }
   private_endpoints_subnet_name = "PrivateEndpointsSubnet"
-  
 }
 
 module "security" {
   source = "../../modules/security"
-
   resource_group_name = azurerm_resource_group.security_rg.name
   location            = azurerm_resource_group.security_rg.location
   tags                = local.common_tags
-  key_vault_name      = "prd-Paybase-kv"
+  key_vault_name      = "voltica-prod-kv-prod882" # <-- Unique name
   vm_admin_password   = var.admin_password
   key_vault_admin_object_ids = [data.azurerm_client_config.current.object_id]
 }
 
-
- module "sql_vm" {
+module "sql_vm" {
   source = "../../modules/sql_vm"
-
-   resource_group_name = azurerm_resource_group.vm_rg.name
-   location            = azurerm_resource_group.vm_rg.location
-   tags                = local.common_tags
-   vm_name        = "Paybasesqlstg"
-   vm_size        = "Standard_B2ms"
-   admin_username = "hswerWjdbds23"
-   admin_password = var.admin_password
-   public_ip_name = "Paybase-stg-vm-pip"
-   nsg_name       = "Paybasesqlstg-nic-nsg"
-   subnet_id      = module.networking.subnet_ids["sqlVmSubnet"]
-   data_disks = [
-     {
-       name          = "sql-data-disk-1"
-       disk_size_gb  = 32
-       lun           = 0
-       caching       = "ReadWrite" # Typical for SQL data disks
-       create_option = "Empty"
-       storage_account_type = "Standard_LRS" # Or "Premium_LRS" for better performance
-     },
-     {
-       name          = "sql-data-disk-2"
-       disk_size_gb  = 32
-       lun           = 1
-       caching       = "ReadOnly" # Typical for SQL log disks, or ReadWrite if also data
-       create_option = "Empty"
-       storage_account_type = "Standard_LRS"
-     }
-   ]
-   
-
- }
+  resource_group_name = azurerm_resource_group.vm_rg.name
+  location            = azurerm_resource_group.vm_rg.location
+  tags                = local.common_tags
+  vm_name             = "volticasqlprod" # <-- Name based on Voltica
+  vm_size             = "Standard_B2ms"
+  admin_username      = "voltica_admin"
+  admin_password      = var.admin_password
+  public_ip_name      = "voltica-prod-vm-pip" # <-- Name based on Voltica
+  nsg_name            = "volticasqlprod-nic-nsg" # <-- Name based on Voltica
+  subnet_id           = module.networking.subnet_ids["sqlVmSubnet"]
+  data_disks = [
+    {
+      name                 = "sql-data-disk-1"
+      disk_size_gb         = 32
+      lun                  = 0
+      caching              = "ReadWrite"
+      create_option        = "Empty"
+      storage_account_type = "Standard_LRS"
+    },
+    {
+      name                 = "sql-data-disk-2"
+      disk_size_gb         = 32
+      lun                  = 1
+      caching              = "ReadOnly"
+      create_option        = "Empty"
+      storage_account_type = "Standard_LRS"
+    }
+  ]
+}
 
 # module "integration_vm_stg" {
+
 #   source = "../../modules/integration_vm_stg" 
 
+
+
 #   resource_group_name = azurerm_resource_group.vm_rg.name
+
 #   location            = azurerm_resource_group.vm_rg.location
+
 #   tags                = local.common_tags
+
 #   vm_name        = "PaybaseintegVm" # Unique name for the new VM
+
 #   vm_size        = "Standard_B2s"    # Example: different size
+
 #   admin_username = "RsgjhsdEintegdfdh"
+
 #   admin_password = var.admin_password
+
 #   public_ip_name = "Paybase-prd-integration-vm-pip"
+
 #   nsg_name       = "PaybaseintegprdVm-nic-nsg"
+
 #   subnet_id      = module.networking.subnet_ids["IntegrationvmSubnet"]
+
 # }
+
 module "app_platform" {
   source = "../../modules/app_platform"
-  resource_group_name       = azurerm_resource_group.app_rg.name
-  location                  = azurerm_resource_group.app_rg.location
-  tags                      = local.common_tags
-  app_service_plan_name     = "Paybase-prd-ASP"
-  acr_name                  = "Paybaseacr"
-  storage_account_name      = "paybasestorageprd"
-  servicebus_namespace_name = "Paybasesb"
-  app_service_plan_sku      = "P1v2"
-  acr_sku                   = "Premium"
-  app_insights_name         = "Paybase-appinsides"
-  app_service_docker_images  = local.app_service_docker_images
-  docker_image_tag           = var.docker_image_tag
-  function_app_names         = local.function_app_names
-  private_endpoint_subnet_id = module.networking.private_endpoint_subnet_id
-  private_dns_zone_ids       = module.networking.private_dns_zone_ids
-
-
+  resource_group_name         = azurerm_resource_group.app_rg.name
+  location                    = azurerm_resource_group.app_rg.location
+  tags                        = local.common_tags
+  app_service_plan_name       = "voltica-prod-asp" # <-- Name based on Voltica
+  acr_name                    = "volticaacrprod882" # <-- Unique name
+  storage_account_name        = "volticastorageprod882" # <-- Unique name
+  servicebus_namespace_name   = "volticasbprod882" # <-- Unique name
+  app_service_plan_sku        = "P1v2"
+  acr_sku                     = "Premium"
+  app_insights_name           = "voltica-prod-appinsides" # <-- Name based on Voltica
+  app_service_docker_images   = local.app_service_docker_images
+  docker_image_tag            = var.docker_image_tag
+  function_app_names          = local.function_app_names
+  private_endpoint_subnet_id  = module.networking.private_endpoint_subnet_id
+  private_dns_zone_ids        = module.networking.private_dns_zone_ids
 }
