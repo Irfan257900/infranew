5b7732da63d65553af0ee8077704584faa2f84cd
Mohammad Irfan <irfanmd40351@gmail.com>
2025-11-07 12:17:35 +0530

    Update storage account name in backend.tf

--- environments/tst/backend.tf
+++ environments/tst/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "VolticTf-RG"
-    storage_account_name = "volticatfstorage"
+    storage_account_name = "volticatfstorage77123"
     container_name       = "tfstate"
     key                  = "tst.terraform.tfstate" 
   }
+
 }
