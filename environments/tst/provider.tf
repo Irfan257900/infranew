193cbc7c8381ac5514b8476bc035f4584f11d79d
Mohammad Irfan <irfanmd40351@gmail.com>
2025-11-08 17:34:01 +0530

    Add SendGrid provider to Terraform configuration
    Added SendGrid provider configuration with API key.

--- environments/tst/provider.tf
+++ environments/tst/provider.tf
@@ -1,14 +1,22 @@
-# This file defines the required providers and their configuration.
-
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = "~> 3.90.0"
+    }
+    # --- ADD THIS NEW BLOCK ---
+    sendgrid = {
+      source  = "sendgrid/sendgrid"
+      version = "~> 0.2.1" # Use the latest version
     }
   }
 }
 
 provider "azurerm" {
   features {}
+}
+
+# --- ADD THIS NEW PROVIDER CONFIG ---
+provider "sendgrid" {
+  api_key = var.sendgrid_api_key
 }
