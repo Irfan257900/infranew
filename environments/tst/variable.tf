decaa73a97cf224b0846aa448b7ff3331bd39131
Mohammad Irfan <irfanmd40351@gmail.com>
2025-11-10 10:39:18 +0530

    Update variable.tf

--- environments/tst/variable.tf
+++ environments/tst/variable.tf
@@ -27,14 +27,5 @@ variable "web_app_names" {
   default     = []
 
 }
-variable "sendgrid_api_key" {
-  description = "Master SendGrid API Key"
-  type        = string
-  sensitive   = true
-}
 
-variable "sendgrid_template_id" {
-  description = "The ID of the SendGrid email template (e.g., d-123...)"
-  type        = string
-  sensitive   = true
-}
+
