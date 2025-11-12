c9b020626176ef3a27d56a36c2789c67b5d800f7
Mohammad Irfan <irfanmd40351@gmail.com>
2025-11-12 13:00:15 +0530

    Update backend.tf

--- environments/tst/backend.tf
+++ environments/tst/backend.tf
@@ -1,10 +1,11 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "VolticTfdemo-RG"
-    storage_account_name = "volticatfstorage771231"
+    resource_group_name  = "VolticTf-RG"
+    storage_account_name = "volticatfstorage77123"
     container_name       = "tfstate"
     key                  = "tst.terraform.tfstate" 
   }
 
 }
+
 
